85919d5969e13715c9b7463b2ae9ea60b2718708
Aleksey Belchenkov <u608110@gmail.com>
2018-05-22 23:12:28 +0300

    Форма для регистрации

--- src/components/Auth/Registration.vue
+++ src/components/Auth/Registration.vue
@@ -1,17 +1,91 @@
 <template>
-  <v-container>
-    <v-layout row>
-      <v-flex xs12>
-        <h1>Регистрация</h1>
+  <v-container fluid fill-height>
+    <v-layout align-center justify-center>
+      <v-flex xs12 sm8 md6>
+        <v-card class="elevation-15">
+          <v-toolbar dark color="info">
+            <v-toolbar-title>Регистрация</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form v-model="valid" ref="form" lazy-validation>
+              <v-text-field
+                prepend-icon="email"
+                v-model="email"
+                color="info"
+                name="login"
+                label="Email"
+                type="email"
+                :rules="emailRules"
+              ></v-text-field>
+              <v-text-field
+                id="password"
+                v-model="password"
+                prepend-icon="lock"
+                color="info"
+                name="password"
+                label="Пароль"
+                type="password"
+                :rules="passRules"
+                :counter="passCount"
+              ></v-text-field>
+              <v-text-field
+                v-model="confirmPassword"
+                prepend-icon="lock"
+                color="info"
+                name="confirm_password"
+                label="Повторите пароль"
+                type="password"
+                :rules="confirmPassRules"
+                :counter="passCount"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="info"
+              @click="onSibmit"
+            >Создать аккаунт</v-btn>
+          </v-card-actions>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+
   export default {
     data () {
-      return {}
+      return {
+        passCount: 6,
+        email: '',
+        confirmPassword: '',
+        valid: '',
+        passRules: [
+          v => !!v || 'Обязательное поле',
+          v => v.length >= this.passCount || 'Пароль должен быть не менее ' + this.passCount
+        ],
+        emailRules: [
+          v => !!v || 'Обязательное поле',
+          v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Некорректный E-mail'
+        ],
+        confirmPassRules: [
+          v => !!v || 'Обязательное поле',
+          v => v === this.password || 'Пароли не совпадают'
+        ]
+      }
+    },
+    methods: {
+      onSibmit: function () {
+        if (this.$refs.form.validate()) {
+          const user = {
+            email: this.email,
+            password: this.password
+          }
+          console.log(user)
+        }
+      }
     }
   }
 </script>
