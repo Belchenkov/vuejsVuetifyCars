e520a0357db6e2d2af54e7827aaf9cae2b9f9c95
Aleksey Belchenkov <u608110@gmail.com>
2018-05-27 18:35:35 +0300

    Страница заказоы

--- src/components/User/Orders.vue
+++ src/components/User/Orders.vue
@@ -1,8 +1,34 @@
 <template>
   <v-container>
     <v-layout row>
-      <v-flex xs12>
-        <h1>Заказы</h1>
+      <v-flex xs12 sm6 offset-sm-3>
+        <h1 class="text--secondary mb-3">Заказы</h1>
+        <v-list two-line subheader>
+          <v-list-tile
+            avatar
+            v-for="order in orders"
+            :key="order.id"
+          >
+            <v-list-tile-action>
+              <v-checkbox
+                @change="markDone(order)"
+                :input-value="order.done"
+                color="info"
+              ></v-checkbox>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>{{ order.name }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ order.phone }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn
+                class="elevation-6 info"
+                small
+                :to="'/ad/' + order.adId"
+              >Подробнее</v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
       </v-flex>
     </v-layout>
   </v-container>
@@ -11,7 +37,22 @@
 <script>
   export default {
     data () {
-      return {}
+      return {
+        orders: [
+          {
+            id: '23423',
+            name: 'Name',
+            phone: '8-927-12-34',
+            adId: '123',
+            done: false
+          }
+        ]
+      }
+    },
+    methods: {
+      markDone (order) {
+        order.done = true
+      }
     }
   }
 </script>
